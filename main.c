c33be7f5855bcc4c3646cf2fe87e2300be0192da
Carlos <carlos.lima@udla.edu.ec>
2026-07-04 22:52:38 -0500

    agregue las estructuras que vamos a usar

--- main.c
+++ main.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 #include <string.h>
 
-
+typedef struct
+{
+    char nombre[50];
+    char especialidad[30];
+    char letra;
+} Doctor;
+ 
+typedef struct
+{
+    char codigo[16];
+    char paciente[50];
+    char especialidad[30];
+    char fecha[11];
+    char hora[6];
+    char medico[50];
+} Cita;
 
 int main()
 {
